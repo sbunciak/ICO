353ecfebca80970f4a24dd8d36705eb15aef71fa
Stefan Bunciak <pipistik.bunciak@gmail.com>
2022-10-11 00:40:03 +0200

    Initial implementation of ICO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x10bBF14ef10658032ada2b387b655Dac980f250A";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
